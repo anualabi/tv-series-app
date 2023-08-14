43f509f571d7b2c8e1770ae62187dc78180baf1f
Anu Alabi <anuoluwapo.alabi20@gmail.com>
2023-08-14 05:01:45 +0200

    Render series data

--- jest.setup.js
+++ jest.setup.js
@@ -1,16 +1,1 @@
 import '@testing-library/jest-dom/extend-expect';
-
-import 'whatwg-fetch';
-import { server } from './mocks/server';
-
-beforeAll(() => {
-  server.listen();
-});
-
-afterEach(() => {
-  server.resetHandlers();
-});
-
-afterAll(() => {
-  server.close();
-});
--- mocks/handlers.js
+++
@@ -1,8 +0,0 @@
-import { rest } from 'msw';
-import series from '../src/__data__/series.json'
-
-export const handlers = [
-  rest.get('/api/insecure', (req, res, ctx) => {
-    return res(ctx.json(series));
-  }),
-];
